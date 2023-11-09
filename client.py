52a9240beb32e7189adc7660b197a8893b4ccc57
Brad Didier <braddidier13244@gmail.com>
2023-11-09 17:45:03 -0500

    Clean-ups and made send/receive functions

--- client.py
+++ client.py
@@ -1,49 +1,59 @@
 import socket
 
-# --- main ---
-
-host = '127.0.0.1'
-port = 8080
-
+# -- globab variables --
 s = socket.socket()
-shutdown = False
 
-print('Client Started. Please use %connect to start or %exit')
+# -- functions ---
+def send_message(clientMessage):
+    clientMessage = clientMessage.encode()
+    s.send(clientMessage)
+    
+def receive_message():
+    serverMessage = s.recv(1024)
+    serverMessage = serverMessage.decode()
+    print(serverMessage)
+    return serverMessage
 
-command = ""
-while command != "%connect" and (not shutdown):
-    command = input("Enter a command: ")
-    if command == "%connect":
-        s.connect((host, port))
-        print("Successfully connected to server")
-        
-        message = "Connected"
-        print('send:', message)
-        message = message.encode()
-        s.send(message)
+# --- main ---
+def main():
+    host = '127.0.0.1'
+    port = 8080
+    
+    shutdown = False
 
-        message = s.recv(1024)
-        message = message.decode()
-        print(message)
+    print('Client Started. Please use %connect to start or %exit')
+
+    command = ""
+    while command != "%connect" and (not shutdown):
+        command = input("Enter a command: ")
+        if command == "%connect":
+            s.connect((host, port))
+            print("Successfully connected to server")
         
-        username = input()
-        username = username.encode()
-        s.send(username)
+            messageToServer = "Connected"
+            send_message(messageToServer)
+
+            serverMessage = receive_message()
         
-        while message.startswith("Error"):
             username = input()
-            username = username.encode()
-            s.send(username)
+            send_message(username)
             
-        message = s.recv(1024)
-        message = message.decode()
-        print(message)
+            serverMessage = receive_message()
         
-    elif command == "%exit":
-        print("Client will be terminated")
-        shutdown = True
-    else:
-        print("Invalid command")
-        command = ""
+            while serverMessage.startswith("Error"):
+                username = input()
+                send_message(username)
+                serverMessage = receive_message()
+            
+            serverMessage = receive_message()
+        
+        elif command == "%exit":
+            print("Client will be terminated")
+            shutdown = True
+        else:
+            print("Invalid command")
+            command = ""
         
         
+if __name__ == '__main__':
+    main()
--- server.py
+++ server.py
@@ -2,50 +2,51 @@ import socket
 import threading
 
 # Global Variables
-usernames = ["UsernameError"] #possible race condition if clients enter usernames in different order than connected
+usernames = ["UsernameError"] #possible race condition if clients enter usernames in different order than connected?
 threads = [0]
 
 # --- functions ---
 
+#send a message to client. Client is determind by information passed into connectionInfo array
 def send_message(connectionInfo, serverMessage):
     serverMessage = serverMessage.encode()
     connectionInfo[0].send(serverMessage)
     print(connectionInfo[2], "client:", connectionInfo[1], 'send:', serverMessage)
+    
+#receive a message from a connected client. Client to listen to is determind by information passed into connnectionInfo array
+def receive_message(connectionInfo):
+    clientMessage = connectionInfo[0].recv(1024)
+    clientMessage = clientMessage.decode()
+    print(connectionInfo[2], "client:", connectionInfo[1], 'recv:', clientMessage)
+    return clientMessage
 
+#runs each time a client connects
 def handle_client(conn, addr):
     threads.append(threads[-1] + 1)
     threadNumber = threads[-1]
     print(threads[threadNumber], "starting")
-
-    # recv message
-    clientMessage = conn.recv(1024)
-    clientMessage = clientMessage.decode()
-    print(threadNumber, "client:", addr, 'recv:', clientMessage)
     
     connectionInfo = [conn, addr, threadNumber]
+
+    clientMessage = receive_message(connectionInfo)
     
     if clientMessage == "Connected":
-        send_message(connectionInfo, "Enter a username to connect:")
+        send_message(connectionInfo, "Enter a username to connect:") #tell client to input username
         
-        clientMessage = conn.recv(1024)
-        clientMessage = clientMessage.decode()
-        print(threadNumber, "client:", addr, 'recv:', clientMessage)
+        clientMessage = receive_message(connectionInfo) #receive inputted username from client side
         
+        #if username exists, tell client to pick another username
         while clientMessage in usernames:
             send_message(connectionInfo, "Error: Username already in use. \nPlease provide valid username:")
-            clientMessage = conn.recv(1024)
-            clientMessage = clientMessage.decode()
-            print(threadNumber, "client:", addr, 'recv:', clientMessage)
+            clientMessage = receive_message(connectionInfo)
             
+        #add new user's username to usernames array
         usernames.append(clientMessage)
         send_message(connectionInfo, "Welcome to the server.")
                 
-    # simulate longer work
-    #time.sleep(5)
-    
     conn.close()
 
-    print("[thread] ending")
+    print(threads[threadNumber], "ending")
    
 # --- main ---
 
