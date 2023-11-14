fc734b71acca7e602f901cc05ad8dcf20880b6d5
Brad Didier <braddidier13244@gmail.com>
2023-11-14 00:12:02 -0500

    added %users, %post, and %leave commands

--- client.py
+++ client.py
@@ -13,14 +13,21 @@ class ReaderThread(threading.Thread):
     def __init__(self, socket):
         super().__init__()
         self.socket = socket
+        self._stop_event = threading.Event()
+        
+    def stop(self):
+        self._stop_event.set()
+        
+    def stopped(self):
+        return self._stop_event.is_set()
         
     def run(self):
         global serverMessage
         try:
-            while True:
+            while not self.stopped():
                 #Wait for a message
                 serverMessage = self.socket.recv(1024).decode()
-                if serverMessage.startswith("NOTICE"):
+                if serverMessage.startswith("EXIT_NOTICE") or serverMessage.startswith("NEW_POST") or serverMessage.startswith("LEAVE_NOTICE"):
                     print('\n' + serverMessage)
         except Exception as e:
             print(f"Exception: {e}")
@@ -41,8 +48,8 @@ def receive_message():
 def help():
     print('Available Commands:')
     print('"%join" --> joins the default group message board (Group 1)')
-    print('"%post subject content" --> posts a message to the board')
-    print('"%users" --> retrieves a list of all users in the group')
+    print('"%post subject content" --> posts a message to the default board (Group 1)')
+    print('"%users" --> retrieves a list of all users in default group (Group 1)')
     print('"%message messageID" --> retrieves contents of specified message')
     print('"%leave" --> leaves the current group')
     print('"%exit" --> disconnect from the server and shutdown the client')
@@ -78,6 +85,51 @@ def runClient(username):
                 print("Successfully joined Group 1")
             elif serverMessage == "GROUP_JOIN_ERROR":
                 print("Error: You may already be in this group")
+            else:
+                print("Error: Unexpected response from server")
+    
+        #users command - currently bugged
+        elif commandList[0] == "%users":
+            print("Getting list of users in Group 1...")
+            send_message("USERS " + username)
+            serverMessageList = serverMessage.split(" ")
+            if serverMessageList[0] == "USERS_ERROR":
+                print("Error: Please join Group 1 to view its users")
+            elif serverMessageList[0] == "SENDING_USERS":
+                numUsers = serverMessageList[1]
+                print("Users in Group 1:")
+                for i in range(int(numUsers)):
+                    print('\n' + serverMessage)
+                    i += 1
+            else:
+                print("Error: Unexpected response from server")  
+                
+        #post command
+        elif commandList[0] == "%post":
+            if len(commandList) != 3:
+                print("Error: Please provide 2 arguments to %post command")
+            else:
+                subject = commandList[1]
+                content = commandList[2]
+                print("Posting message...")
+                send_message("POST " + subject + " " + content)
+                wait_for_server()
+                if serverMessage == "POST_ERROR":
+                    print("Error: Please join Group 1 to post in it")
+                elif serverMessage.startswith("NEW_POST"):
+                    print("Your message was successfully posted in the group")
+                else:
+                    print("Error: Unexpected response from server")
+                    
+        #leave command
+        elif commandList[0] == "%leave":
+            print("Leaving group...")
+            send_message("LEAVE " + username)
+            wait_for_server()
+            if serverMessage.startswith("LEAVE_NOTICE"):
+                print("You have been removed from the group")
+            elif serverMessage == "LEAVE_ERROR":
+                print("Error: You must be in a group to leave it")
             else:
                 print("Error: Unexpected response from server")
                 
--- server.py
+++ server.py
@@ -1,5 +1,9 @@
 import socket
 import threading
+import time
+import json
+import random
+from datetime import datetime
 
 # Global Variables
 activeConnections = []
@@ -11,6 +15,8 @@ group2Users = []
 group3Users = []
 group4Users = []
 group5Users = []
+groups = [group1Users, group2Users, group3Users, group4Users, group5Users]
+messages = []
 
 # --- functions ---
 
@@ -20,6 +26,12 @@ def send_message(connectionInfo, serverMessage):
     connectionInfo[0].send(serverMessage)
     print(connectionInfo[2], "client:", connectionInfo[1], 'send:', serverMessage)
     
+def send_array_message(connectionInfo, arrayToSend):
+    for item in arrayToSend:
+        serverMessage = item.encode()
+        connectionInfo[0].send(serverMessage)
+        print(connectionInfo[2], "client:", connectionInfo[1], 'send:', serverMessage)
+    
 #receive a message from a connected client. Client to listen to is determind by information passed into connnectionInfo array
 def receive_message(connectionInfo):
     clientMessage = connectionInfo[0].recv(1024)
@@ -27,8 +39,9 @@ def receive_message(connectionInfo):
     print(connectionInfo[2], "client:", connectionInfo[1], 'recv:', clientMessage)
     return clientMessage
 
-#send a given message to all active clients
-#this successfully sends a message, but how do we get clients to always be listening for a NOTICE message? 
+#send a given message to all active clients 
+#actually, this should be rewritten to only send messages to active clients WITHIN THE SAME group as current operation
+#for now, while only using 1 group, it's cool i guess
 def send_to_all(message):
     for conn in activeConnections:
         i = 0
@@ -38,7 +51,6 @@ def send_to_all(message):
 #to be called when a client disconnects. Scrubs them from all arrays that keep track of them
 def cleanup(connectionInfo, username):
     print(threads[connectionInfo[2]], "ending")
-    groups = [group1Users, group2Users, group3Users, group4Users, group5Users]
     for group in groups:
         if username in group:
             group.remove(username)
@@ -46,6 +58,22 @@ def cleanup(connectionInfo, username):
     activeAddresses.remove(connectionInfo[1])
     threads.remove(connectionInfo[2])
     usernames.remove(username)
+    
+def create_message(username, subject, content):
+    messageId = random.randint(1000, 9999)
+    postDate = datetime.now().isoformat()
+    message = {
+        "ID": messageId,
+        "Sender": username,
+        "Post Date": postDate,
+        "Subject": subject,
+        "Content": content
+    }
+    #store entire message, including content to be retrieved later
+    messages.append(message)
+    #pop the content field off of the json object before it is sent to post
+    message.popitem()
+    return json.dumps(message)
 
 #runs each time a client connects
 def handle_client(conn, addr):
@@ -80,12 +108,15 @@ def handle_client(conn, addr):
             
             clientMessageList = clientMessage.split(" ")
             
+            #handle %exit command
             if clientMessageList[0] == "EXIT":
                 print(currentUsername, "has left the server")
                 cleanup(connectionInfo, currentUsername)
-                send_to_all("NOTICE: " + currentUsername + " has disconnected from the server")
+                send_to_all("EXIT_NOTICE: " + currentUsername + " has disconnected from the server")
                 clientConnected = False
                 break
+            
+            #handle %join command
             elif clientMessageList[0] == "JOIN":
                 print(currentUsername, "wants to join Group 1")
                 if currentUsername not in group1Users:
@@ -95,8 +126,47 @@ def handle_client(conn, addr):
                 else:
                     print(currentUsername, "tried to join group 1, but is already in it")
                     send_message(connectionInfo, "GROUP_JOIN_ERROR")
+                    
+            #currently bugged
             elif clientMessageList[0] == "USERS":
-                print(currentUsername, "wants a list of users")
+                print(currentUsername, "wants a list of users in their group")
+                if currentUsername in group1Users:
+                    print("Sending users in Group 1")
+                    send_message(connectionInfo, "SENDING_USERS " + str(len(group1Users)))
+                    send_array_message(connectionInfo, group1Users)
+                else:
+                    print(currentUsername, "tried to get group 1 users, but is not in the group")
+                    send_message(connectionInfo, "USERS_ERROR")
+                    
+            #handle %post command
+            elif clientMessageList[0] == "POST":
+                subject = clientMessageList[1]
+                content = clientMessageList[2]
+                print(currentUsername, "wants to post in Group 1")
+                if currentUsername in group1Users:
+                    message = create_message(currentUsername, subject, content)
+                    print("Posting message...")
+                    send_to_all("NEW_POST" + message)
+                else:
+                    print(currentUsername, "tried to post a message to group 1, but isn't in it")
+                    send_message(connectionInfo, "POST_ERROR")
+                    
+            elif clientMessageList[0] == "LEAVE":
+                count = 0
+                for group in groups:
+                    if currentUsername in group:
+                        group.remove(currentUsername)
+                        print(currentUsername, "has left the group")
+                        send_to_all("LEAVE_NOTICE: " + currentUsername + " has left the group")
+                    else:
+                        count += 1
+                #looked in all the groups, didn't find the user
+                if count == 5:
+                    send_message(connectionInfo, "LEAVE_ERROR")
+                    
+                        
+                
+                
                 
     conn.close()
    
